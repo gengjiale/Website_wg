824f0483a8310db0c148ee3a5e3b5f342cca5ee7
root <976752764@qq.com>
2024-10-12 16:19:18 +0800

    修改nacos配置错误

--- back/build.sh
+++ back/build.sh
@@ -1,16 +1,18 @@
-#!/bin/sh
+## 该文件已废弃
 
-export jenkins_home=/var/jenkins_home
-export usst_website_back=$jenkins_home/workespace/usst_website/back
-# shellcheck disable=SC2164
-cd $jenkins_home
-# 创建挂载目录
-if [ ! -e volumes/usst_website ]; then
-  mkdir -p volumes/usst_website
-  # shellcheck disable=SC2164
-  cd volumes/usst_website
-  mkdir -p mysql nginx nacos
-fi
-
-export usst_website_volumes=$jenkins_home/volumes/usst_website
+##!/bin/sh
+#
+#export jenkins_home=/var/jenkins_home
+#export usst_website_back=$jenkins_home/workespace/usst_website/back
+## shellcheck disable=SC2164
+#cd $jenkins_home
+## 创建挂载目录
+#if [ ! -e volumes/usst_website ]; then
+#  mkdir -p volumes/usst_website
+#  # shellcheck disable=SC2164
+#  cd volumes/usst_website
+#  mkdir -p mysql nginx nacos
+#fi
+#
+#export usst_website_volumes=$jenkins_home/volumes/usst_website
 
