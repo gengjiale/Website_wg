f33e4948fcb6ef205abe716298421d4ca2f33431
root <976752764@qq.com>
2024-10-12 17:04:40 +0800

    修改nacos配置错误

--- back/nacos-mysql.sql
+++ back/nacos-mysql.sql
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-USE nacos;
 /******************************************/
 /*   表名称 = config_info                  */
 /******************************************/
