ee319c17ff3ffb43c15bb85fa552fbbaa825d617
root <976752764@qq.com>
2024-10-14 15:27:35 +0800

    测试mockjs

--- front/test/src/main.js
+++ front/test/src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-// import './utils/mock.js'
+import './utils/mock.js'
 
 
 createApp(App).mount('#app')
--- front/test/src/utils/mock.js
+++ front/test/src/utils/mock.js
@@ -1,6 +1,2 @@
 import Mock from 'mockjs'
-Mock.mock('http://47.96.179.141/api/v1/test','get',()=>{
-    return Mock.mock({
-        'message': "测试请求拦截"
-    })
-})
+Mock.mock('http://47.96.179.141/api/v1/test','get',"请求测试拦截")
