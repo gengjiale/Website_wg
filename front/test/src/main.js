e4bbe15f7183c00998530b70d368e36b1a457222
root <976752764@qq.com>
2024-10-14 15:44:46 +0800

    为前端区分开发和生产环境

--- front/test/src/main.js
+++ front/test/src/main.js
@@ -1,7 +1,11 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import './utils/mock.js'
+
+if ( process.env.NODE_ENV === "development" ){
+   import('./utils/mock.js' )
+}
+
 
 
 createApp(App).mount('#app')
